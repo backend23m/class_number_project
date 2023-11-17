9d16511573bb554b6d368c06e66268aa77e41f97
musayevjahongir <musayevjahongir5051@gmail.com>
2023-11-17 10:59:48 +0500

    metodlar

--- number.py
+++ number.py
@@ -9,7 +9,7 @@ class Number:
 
         returns: int
         """
-        pass
+        return self.value
 
     def is_odd(self):
         """
@@ -18,7 +18,7 @@ class Number:
         returns: bool
 
         """
-        pass
+        return self.value%2==1
 
     def is_even(self):
         """
@@ -26,7 +26,7 @@ class Number:
 
         returns: bool
         """
-        pass
+        return not self.is_odd()
 
     def is_prime(self):
         """
@@ -34,15 +34,24 @@ class Number:
 
         returns: bool
         """
-        pass
-
+        s=0
+        for i in range(self.value//2):
+            if self.value%i:
+                s+=1
+        return s==1
     def get_divisors(self):
         """
         Returns a list of all the divisors of the number.
 
         returns: list
         """
-        pass
+        l=[]
+        for i in range(self.value//2):
+            if self.value%i==0:
+                l.append(i)
+        l.append(self.value)
+        return l
+
 
     def get_length(self):
         """
